2fdc8e8b98fd56bc65da4685bb5596b50a9d9669
Avishek Dangol <dangolavishek@gmail.com>
2023-05-23 11:54:22 +0545

    navbar

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    semi: ['error', 'never'],
+    'max-len': 'off',
+    'linebreak-style': 'off',
+    camelcase: ['error', { properties: 'never', ignoreDestructuring: true, ignoreImports: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'vue/multiline-html-element-content-newline': 'off',
+  },
+}
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import logo from '../logo.svg'
+
+function NavBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg">
+      <Container>
+        <Nav>
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">Portfolio</Nav.Link>
+        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Navbar.Brand href="#home">
+              <img src={logo} alt="logo" />
+              Portfolio
+            </Navbar.Brand>
+          </Nav>
+          <Nav>
+            <div>
+              <small className="d-block">Call: +977-9812131415</small>
+              <small>Write: kumud@abc.com</small>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
